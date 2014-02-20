00eae2915d598fefe773020ccf865b56da54e0d0
Yiwei Chen <yiwei.ch@live.com>
2014-02-20 22:24:48 +0000

    update

--- dtn.c
+++ dtn.c
@@ -20,20 +20,20 @@
 #define DTN_DEBUG_LEVEL 2
 
 #if DTN_DEBUG_LEVEL >= 2
-#define __(...) printf(__VA_ARGS__)
-#define __ADDR(addr) printf("%02x:%02x", (addr)->u8[1], (addr)->u8[0])
-#define _(...) printf(__VA_ARGS__)
-#define _ADDR(addr) printf("%02x:%02x", (addr)->u8[1], (addr)->u8[0])
+#define INFO(...) printf(__VA_ARGS__)
+#define INFOADDR(addr) printf("%02x:%02x", (addr)->u8[1], (addr)->u8[0])
+#define IMPT(...) printf(__VA_ARGS__)
+#define IMPTADDR(addr) printf("%02x:%02x", (addr)->u8[1], (addr)->u8[0])
 #elif DTN_DEBUG_LEVEL == 1
-#define __(...)
-#define __ADDR(addr)
-#define _(...) printf(__VA_ARGS__)
-#define _ADDR(addr) printf("%02x:%02x", (addr)->u8[1], (addr)->u8[0])
+#define INFO(...)
+#define INFOADDR(addr)
+#define IMPT(...) printf(__VA_ARGS__)
+#define IMPTADDR(addr) printf("%02x:%02x", (addr)->u8[1], (addr)->u8[0])
 #else
-#define __(...)
-#define __ADDR(addr)
-#define _(...)
-#define _ADDR(addr)
+#define INFO(...)
+#define INFOADDR(addr)
+#define IMPT(...)
+#define IMPTADDR(addr)
 #endif
 
 #define DTN_PENDING (void*)(0)
@@ -47,15 +47,15 @@ PACKETQUEUE(dtn_packetqueue, DTN_QUEUE_MAX);
 void
 print_packetbuf(char * func)
 {
-  __("%s: total length %d\n", func, packetbuf_totlen());
+  INFO("%s: total length %d\n", func, packetbuf_totlen());
   struct dtn_hdr *hdrptr = dtn_buf_ptr();
-  __("    packetbuf_hdr: {l: %d", hdrptr->num_copies);
-  __(", es: ");
-  __ADDR(&(hdrptr->esender));
-  __(", er: ");
-  __ADDR(&(hdrptr->ereceiver));
-  __(", ep: %d}\n", hdrptr->epacketid);
-  __("    packetbuf_data: {len: %d, s: %s}\n",
+  INFO("    packetbuf_hdr: {l: %d", hdrptr->num_copies);
+  INFO(", es: ");
+  INFOADDR(&(hdrptr->esender));
+  INFO(", er: ");
+  INFOADDR(&(hdrptr->ereceiver));
+  INFO(", ep: %d}\n", hdrptr->epacketid);
+  INFO("    packetbuf_data: {len: %d, s: %s}\n",
        packetbuf_totlen() - sizeof(struct dtn_hdr),
        hdrptr + 1);
 }
@@ -73,17 +73,17 @@ void
 dtn_queue_spray(void *ptr)
 {
   struct dtn_conn *c = (struct dtn_conn *)ptr;
-  _("dtn_queue_spray: Spraying, queue length: %d\n", packetqueue_len(c->q));
+  IMPT("dtn_queue_spray: Spraying, queue length: %d\n", packetqueue_len(c->q));
   
   if (packetqueue_len(c->q) <= 0) {
-    _("dtn_queue_spray: Empty packetqueue, nothing to spray, stopped.\n");
+    IMPT("dtn_queue_spray: Empty packetqueue, nothing to spray, stopped.\n");
     return;
   }
   
   struct packetqueue_item *q_item;
   for (q_item = packetqueue_first(c->q); q_item; q_item = q_item->next) {
     if (q_item->ptr != DTN_READY) {
-      __("dtn_queue_spray: packet still pending, skip.\n");
+      INFO("dtn_queue_spray: packet still pending, skip.\n");
       continue;
     }
     struct dtn_hdr *qbufdata = (struct dtn_hdr *)
@@ -94,15 +94,15 @@ dtn_queue_spray(void *ptr)
     queuebuf_to_packetbuf(packetqueue_queuebuf(q_item));
     print_packetbuf("dtn_queue_spray");
     broadcast_send(&c->spray_c);
-    __("dtn_queue_spray: broadcast Spray sent.\n");
+    INFO("dtn_queue_spray: broadcast Spray sent.\n");
   }
   
-  __("dtn_queue_spray: Paused spraying.\n");
+  INFO("dtn_queue_spray: Paused spraying.\n");
   if (ctimer_expired(&c->spray_ct)){
     ctimer_set(&c->spray_ct,
                DTN_SPRAY_DELAY * CLOCK_SECOND,
                dtn_queue_spray, (void *)c);
-    __("dtn_queue_spray: Timer started.\n");
+    INFO("dtn_queue_spray: Timer started.\n");
   }
 }
 /*---------------------------------------------------------------------------*/
@@ -133,7 +133,7 @@ dtn_valid_hdr()
     return 1;
   } else {
     print_packetbuf("dtn_valid_hdr");
-    _("dtn_valid_hdr: packet invalid.\n");
+    IMPT("dtn_valid_hdr: packet invalid.\n");
     return 0;
   }
 }
@@ -141,7 +141,7 @@ dtn_valid_hdr()
 void
 dtn_spray_recv(struct broadcast_conn *b_c, const rimeaddr_t *from)
 {
-  __("dtn_spray_recv: broadcast received from %02x:%02x.\n",
+  INFO("dtn_spray_recv: broadcast received from %02x:%02x.\n",
        from->u8[1], from->u8[0]);
   if (!dtn_valid_hdr()) return;
   struct dtn_conn *c = (struct dtn_conn *)
@@ -152,14 +152,14 @@ dtn_spray_recv(struct broadcast_conn *b_c, const rimeaddr_t *from)
   memcpy(&recv_hdr, bufdata, sizeof (struct dtn_hdr));
   
   if (rimeaddr_cmp(&(recv_hdr.esender), &rimeaddr_node_addr)) { // from me
-    __("dtn_spray_recv: Spray message is from me, do nothing.\n");
+    INFO("dtn_spray_recv: Spray message is from me, do nothing.\n");
     return;
   }
   
   if (rimeaddr_cmp(&(recv_hdr.ereceiver), &rimeaddr_node_addr)) { // to me
-    _("dtn_spray_recv: Spray message is to me, invoking callback.\n");
+    IMPT("dtn_spray_recv: Spray message is to me, invoking callback.\n");
     unicast_send(&c->request_c, from);
-    _("dtn_spray_recv: unicast Request confirmation sent.\n");
+    IMPT("dtn_spray_recv: unicast Request confirmation sent.\n");
     packetbuf_hdrreduce(sizeof(struct dtn_hdr));
     c->cb->recv(c, &(recv_hdr.esender), recv_hdr.epacketid);
     return;
@@ -170,10 +170,10 @@ dtn_spray_recv(struct broadcast_conn *b_c, const rimeaddr_t *from)
   struct packetqueue_item * item;
   if (item = dtn_queue_find(c)) { // found in the queue
     if (item->ptr == DTN_READY) {
-      __("dtn_spray_recv: Spray in the queue and ready, do nothing.\n");
+      INFO("dtn_spray_recv: Spray in the queue and ready, do nothing.\n");
     } else { // still pending
       unicast_send(&c->request_c, from);
-      _("dtn_spray_recv: Spray in queue but pending, Request sent.\n");
+      IMPT("dtn_spray_recv: Spray in queue but pending, Request sent.\n");
     }
     return;
   }
@@ -183,11 +183,11 @@ dtn_spray_recv(struct broadcast_conn *b_c, const rimeaddr_t *from)
   if (packetqueue_enqueue_packetbuf(c->q,
                                     DTN_MAX_LIFETIME * CLOCK_SECOND,
                                     DTN_PENDING)) {
-    __("dtn_spray_recv: Enqueued (pending) successfully.\n");
+    INFO("dtn_spray_recv: Enqueued (pending) successfully.\n");
     unicast_send(&c->request_c, from);
-    _("dtn_spray_recv: unicast Request sent.\n");
+    IMPT("dtn_spray_recv: unicast Request sent.\n");
   } else {
-    _("dtn_spray_recv: Failed to enqueue.\n");
+    IMPT("dtn_spray_recv: Failed to enqueue.\n");
   }
 }
 /*---------------------------------------------------------------------------*/
@@ -196,7 +196,7 @@ const struct broadcast_callbacks dtn_spray_call = {dtn_spray_recv};
 void
 dtn_request_recv(struct unicast_conn *u_c, const rimeaddr_t *from)
 {
-  __("dtn_request_recv: unicast received from %02x:%02x\n",
+  INFO("dtn_request_recv: unicast received from %02x:%02x\n",
        from->u8[1], from->u8[0]);
   if (!dtn_valid_hdr()) return;
   struct dtn_conn *c = (struct dtn_conn *)
@@ -205,32 +205,32 @@ dtn_request_recv(struct unicast_conn *u_c, const rimeaddr_t *from)
   
   struct packetqueue_item * q_item;
   if (!(q_item = dtn_queue_find(c))) {
-    _("dtn_request_recv: Request not in the queue, do nothing.\n");
+    IMPT("dtn_request_recv: Request not in the queue, do nothing.\n");
     return;
   }
   if (q_item->ptr != DTN_READY) {
-    _("dtn_request_recv: Request in queue, but pending, do nothing.\n");
+    IMPT("dtn_request_recv: Request in queue, but pending, do nothing.\n");
     return;
   }
-  __("dtn_request_recv: Request found in the queue.\n");
+  INFO("dtn_request_recv: Request found in the queue.\n");
   struct dtn_hdr *qbufdata = (struct dtn_hdr *)
                              queuebuf_dataptr(packetqueue_queuebuf(q_item));
   if (rimeaddr_cmp(&(qbufdata->ereceiver), from)) {
     qbufdata->num_copies = 0;
-    _("dtn_request_recv: receiver got message, set L to 0.\n");
+    IMPT("dtn_request_recv: receiver got message, set L to 0.\n");
     return;
   }
   if ((qbufdata->num_copies == 1)
       && !rimeaddr_cmp(from, &(qbufdata->ereceiver))) {
-    _("dtn_request_recv: L == 1, and from != ereceiver, do nothing.\n");
+    IMPT("dtn_request_recv: L == 1, and from != ereceiver, do nothing.\n");
     return;
   }
   if (qbufdata->num_copies == 0) {
-    _("dtn_request_recv: L == 0, do nothing.\n");
+    IMPT("dtn_request_recv: L == 0, do nothing.\n");
     return;
   }
   if (c->handoff_qb != NULL) {
-    _("dtn_request_recv: Another HandOff in progress, do nothing.\n");
+    IMPT("dtn_request_recv: Another HandOff in progress, do nothing.\n");
     return;
   }
   c->handoff_qb = (struct dtn_hdr *)
@@ -240,7 +240,7 @@ dtn_request_recv(struct unicast_conn *u_c, const rimeaddr_t *from)
   struct dtn_hdr *bufdata = dtn_buf_ptr();
   bufdata->num_copies /= 2;
   runicast_send(&c->handoff_c, from, DTN_RTX);
-  _("dtn_request_recv: runicast HandOff(L=%d) sending.\n",
+  IMPT("dtn_request_recv: runicast HandOff(L=%d) sending.\n",
        bufdata->num_copies);
 }
 /*---------------------------------------------------------------------------*/
@@ -250,17 +250,17 @@ void
 dtn_handoff_recv(struct runicast_conn *r_c, const rimeaddr_t *from,
                  uint8_t seqno)
 {
-  __("dtn_handoff_recv: runicast received from %02x:%02x, seqno %d\n",
+  INFO("dtn_handoff_recv: runicast received from %02x:%02x, seqno %d\n",
        from->u8[1], from->u8[0], seqno);
   if (!dtn_valid_hdr()) return;
   struct dtn_conn *c = (struct dtn_conn *)
                        ((void *)r_c - offsetof(struct dtn_conn, handoff_c));
   struct packetqueue_item * q_item;
   if (!(q_item = dtn_queue_find(c))) { // not found in the queue
-    _("dtn_handoff_recv: HandOff not in the queue, do nothing.\n");
+    IMPT("dtn_handoff_recv: HandOff not in the queue, do nothing.\n");
     return;
   }
-  __("dtn_handoff_recv: HandOff found in the queue.\n");
+  INFO("dtn_handoff_recv: HandOff found in the queue.\n");
   struct dtn_hdr *qbufdata = (struct dtn_hdr *)
                              queuebuf_dataptr(packetqueue_queuebuf(q_item));
   struct dtn_hdr *bufdata = dtn_buf_ptr();
@@ -269,8 +269,8 @@ dtn_handoff_recv(struct runicast_conn *r_c, const rimeaddr_t *from,
     qbufdata->num_copies = DTN_L_COPIES;
   }
   q_item->ptr = DTN_READY;
-  __("dtn_handoff_recv: packet state set to ready.\n");
-  _("dtn_handoff_recv: HandOff(L=%d) received and processed.\n",
+  INFO("dtn_handoff_recv: packet state set to ready.\n");
+  IMPT("dtn_handoff_recv: HandOff(L=%d) received and processed.\n",
        bufdata->num_copies);
   dtn_queue_spray((void *)c);
 }
@@ -279,7 +279,7 @@ void
 dtn_handoff_sent(struct runicast_conn *r_c, const rimeaddr_t *to,
                  uint8_t retransmissions)
 {
-  __("dtn_handoff_sent: runicast sent to %02x:%02x, retried %d\n",
+  INFO("dtn_handoff_sent: runicast sent to %02x:%02x, retried %d\n",
        to->u8[1], to->u8[0], retransmissions);
   struct dtn_conn *c = (struct dtn_conn *)
                        ((void *)r_c - offsetof(struct dtn_conn, handoff_c));
@@ -287,9 +287,9 @@ dtn_handoff_sent(struct runicast_conn *r_c, const rimeaddr_t *to,
       && c->handoff_qb->epacketid == c->handoff_hdr.epacketid) {
     int sent_copies = c->handoff_qb->num_copies / 2;
     c->handoff_qb->num_copies = c->handoff_qb->num_copies - sent_copies;
-    _("dtn_handoff_sent: HandOff(L=%d) processed.\n", sent_copies);
+    IMPT("dtn_handoff_sent: HandOff(L=%d) processed.\n", sent_copies);
   } else {
-    _("dtn_handoff_sent: not matched (expired), HandOff not processed.\n");
+    IMPT("dtn_handoff_sent: not matched (expired), HandOff not processed.\n");
   }
   c->handoff_qb = NULL;
 }
@@ -298,12 +298,12 @@ void
 dtn_handoff_timedout(struct runicast_conn *r_c, const rimeaddr_t *to,
                      uint8_t retransmissions)
 {
-  _("dtn_handoff_timedout: runicast timed out, to %02x:%02x, retried %d\n",
+  IMPT("dtn_handoff_timedout: runicast timed out, to %02x:%02x, retried %d\n",
        to->u8[1], to->u8[0], retransmissions);
   struct dtn_conn *c = (struct dtn_conn *)
                        ((void *)r_c - offsetof(struct dtn_conn, handoff_c));
   c->handoff_qb = NULL;
-  _("dtn_handoff_sent: HandOff failed.\n");
+  IMPT("dtn_handoff_sent: HandOff failed.\n");
 }
 /*---------------------------------------------------------------------------*/
 const struct runicast_callbacks dtn_handoff_call = {dtn_handoff_recv,
@@ -322,7 +322,7 @@ dtn_open(struct dtn_conn *c, uint16_t dtn_channel,
   broadcast_open(&c->spray_c, dtn_channel, &dtn_spray_call);
   unicast_open(&c->request_c, dtn_channel + 1, &dtn_request_call);
   runicast_open(&c->handoff_c, dtn_channel + 2, &dtn_handoff_call);
-  _("dtn_open: DTN connection opened at channel (%d, %d, %d).\n",
+  IMPT("dtn_open: DTN connection opened at channel (%d, %d, %d).\n",
        dtn_channel, dtn_channel + 1, dtn_channel + 2);
 }
 /*---------------------------------------------------------------------------*/
@@ -332,14 +332,14 @@ dtn_close(struct dtn_conn *c)
   broadcast_close(&c->spray_c);
   unicast_close(&c->request_c);
   runicast_close(&c->handoff_c);
-  _("dtn_close: DTN closed.");
+  IMPT("dtn_close: DTN closed.");
 }
 /*---------------------------------------------------------------------------*/
 int
 dtn_send(struct dtn_conn *c, const rimeaddr_t *to)
 {
   if (rimeaddr_cmp(to, &rimeaddr_node_addr)) { // to me
-    _("dtn_send: send to myself, invoking callback.\n");
+    IMPT("dtn_send: send to myself, invoking callback.\n");
     c->cb->recv(c, to, c->seqno++);
     return 1;
   }
@@ -358,11 +358,11 @@ dtn_send(struct dtn_conn *c, const rimeaddr_t *to)
   if (packetqueue_enqueue_packetbuf(c->q,
                                     DTN_MAX_LIFETIME * CLOCK_SECOND,
                                     DTN_READY)) {
-    __("dtn_send: Enqueued successfully.\n");
+    INFO("dtn_send: Enqueued successfully.\n");
     dtn_queue_spray((void *)c);
     return 1;
   } else {
-    _("dtn_send: Failed to enqueue.\n");
+    IMPT("dtn_send: Failed to enqueue.\n");
     return 0;
   }
 }
@@ -371,16 +371,16 @@ void
 dtn_set_power(uint8_t power)
 {
   set_power(power);
-  _("dtn_set_power: Power set to 0x%02x.\n", power);
+  IMPT("dtn_set_power: Power set to 0x%02x.\n", power);
 }
 /*---------------------------------------------------------------------------*/
 void
 dtn_set_addr(rimeaddr_t *addr)
 {
   rimeaddr_set_node_addr(addr);
-  _("dtn_set_addr: Local address set to ");
-  _ADDR(addr);
-  _(".\n");
+  IMPT("dtn_set_addr: Local address set to ");
+  IMPTADDR(addr);
+  IMPT(".\n");
 }
 /*---------------------------------------------------------------------------*/
 /** }@ */
